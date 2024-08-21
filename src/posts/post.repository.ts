4818e9a26c179823a39d3bfa3cbd4bcacdf1bea1
June <easdkr@daum.net>
2024-08-21 22:53:09 +0900

    [feat] 딜레이 함수 분리

--- src/posts/post.repository.ts
+++ src/posts/post.repository.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { delay } from 'src/utils';
 
 @Injectable()
 export class PostRepository {
-  public async findIds(length: number, delay = 100): Promise<number[]> {
-    await this.delay(delay);
+  public async findIds(length: number, _delay = 100): Promise<number[]> {
+    await delay(_delay);
     return Array.from({ length }, (_, i) => i + 1);
   }
-
-  private delay = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
 }
